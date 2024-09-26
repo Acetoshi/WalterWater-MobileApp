f88fcb1560f6a3c6e9c1b048ce04cda0ec732acf
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-09-26 18:39:27 +0200

    added webview and eas to build the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,5 @@
-import { Text, View } from "react-native";
+import { WebView } from 'react-native-webview';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function HomeScreen() {
+  return <WebView source={{ uri: "https://walterwater.vercel.app/" }} />;
 }
