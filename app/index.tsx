5eb27f17c42ac8d8a976cded974e6fde9dc785b6
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-09-26 20:16:08 +0200

    started to implement geolocation, popup OK on android

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,51 @@
-import { WebView } from 'react-native-webview';
+import React, { useEffect, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import { WebView } from "react-native-webview";
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      if (Platform.OS === "android") {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "This app needs access to your location.",
+            buttonNeutral: "Ask Me Later",
+            buttonNegative: "Cancel",
+            buttonPositive: "OK",
+          }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log("Didier")
+          getLocation();
+        } else {
+          console.log("Location permission denied");
+        }
+      } else {
+        getLocation();
+      }
+    };
+
+    const getLocation = () => {
+      Geolocation.watchPosition(
+        (position) => {
+          setLocation(position.coords);
+          console.log(location)
+        },
+        (error) => {
+          console.error(error);
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+    };
+
+
+    requestLocationPermission();
+  }, []);
+
   return <WebView source={{ uri: "https://walterwater.vercel.app/" }} />;
 }
