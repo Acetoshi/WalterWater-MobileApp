86aa83f4347d0c6ee8b501747fb17a50517fc243
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-09-28 11:26:02 +0200

    commented dev-only safeguards

--- app/index.tsx
+++ app/index.tsx
@@ -15,12 +15,15 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
+      // Only for dev purposes.
       if (Platform.OS === "android" && !Device.isDevice) {
         setErrorMsg(
           "Oops, this will not work on Snack in an Android Emulator. Try it on your device!"
         );
         return;
       }
+
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
